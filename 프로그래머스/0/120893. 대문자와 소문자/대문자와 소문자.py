3b7abc334a59e8110c19cb56e76c73eada825ec8
Sooyeon Kim <52311972+moominsoo@users.noreply.github.com>
2024-04-04 16:55:35 +0900

    Update 대문자와 소문자.py

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,2 +1,14 @@
 def solution(my_string):
     return my_string.swapcase()
+
+"""
+def solution(my_string):
+    answer = ''
+
+    for i in my_string:
+        if i.isupper():
+            answer+=i.lower()
+        else:
+            answer+=i.upper()
+    return answer 
+    """
