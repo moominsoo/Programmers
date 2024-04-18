585c455dc07e679a20cfafef84a0adefa4687251
Sooyeon Kim <52311972+moominsoo@users.noreply.github.com>
2024-04-18 19:09:39 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,11 @@
+def solution(common):
+    answer=0
+
+    
+    if (common[1]-common[0]==common[2]-common[1]):
+        answer=common[-1] + (common[1]-common[0])
+    elif (common[1]//common[0]==common[2]//common[1]):
+        answer=common[-1]*(common[2]//common[1])
+
+    return answer
+
